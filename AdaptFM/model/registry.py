7ff1ac19fec5c12795a86855a163f3b4dc5287d8
hbakhtiar <hbakhtiar@wisc.edu>
2026-03-30 12:54:10 -0500

    Clear conda_env paths in model registry
    Updated conda_env paths for microSAM and CellposeSAM to empty strings.

--- AdaptFM/model/registry.py
+++ AdaptFM/model/registry.py
@@ -1,13 +1,13 @@
 # gui_scripts/models/registry.py
 from gui_scripts.model.nnUNetV2Spec import NNUNetV2ModelSpec
-from gui_scripts.model.fmSpec import FoundationModelSpec, MicroSAMSpec,CellposeSAMSpec
+from gui_scripts.model.fmSpec import FoundationModelSpec, MicroSAMSpec,CellposeSAMSpec,
 
 MODEL_REGISTRY = {
     "nnUNetv2": NNUNetV2ModelSpec(),
 
     "microSAM": MicroSAMSpec(
         name="microSAM",
-        conda_env="/mnt/local/data3/conda_envs/micro-sam/",
+        conda_env="",
         module_path="micro_sam.training",
         training_wrapper_path = "micro_sam.train_wrapper",
         inference_wrapper_path = "micro_sam.inference_wrapper"
@@ -15,7 +15,7 @@ MODEL_REGISTRY = {
 
     "CellposeSAM": CellposeSAMSpec(
         name="CellposeSAM",
-        conda_env="/mnt/local/data3/conda_envs/cellpose/",
+        conda_env="",
         module_path="cellpose.train",
         training_wrapper_path ="cellpose.train_wrapper",
         inference_wrapper_path = "cellpose.inference_wrapper"
