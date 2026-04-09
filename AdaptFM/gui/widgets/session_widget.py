edd578d7846478eec2747d37c7f6253db7b6d2e2
hbakhtiar <hbakhtiar@wisc.edu>
2026-04-09 11:40:32 -0500

    Update session_widget.py

--- AdaptFM/gui/widgets/session_widget.py
+++ AdaptFM/gui/widgets/session_widget.py
@@ -26,7 +26,7 @@ class SessionWidget:
             self.viewer,
             "Original",
             img,
-            colormap="blue",
+            colormap="gray",
         )
 
     def _build(self, layout):
@@ -43,37 +43,8 @@ class SessionWidget:
             self.session.set_images([path])
             self._load_path(path)
 
-        @magicgui(call_button="Open folder")
-        def open_folder():
-            folder = QFileDialog.getExistingDirectory(
-                None,
-                "Select folder"
-            )
-            if not folder:
-                return
-
-            paths = sorted(glob(os.path.join(folder, "*.tif*")))
-            if not paths:
-                return
-
-            self.session.set_images(paths)
-            self._load_path(paths[0])
-
-        @magicgui(call_button="Previous image")
-        def prev_image():
-            path = self.session.prev()
-            if path:
-                self._load_path(path)
-
-        @magicgui(call_button="Next image")
-        def next_image():
-            path = self.session.next()
-            if path:
-                self._load_path(path)
-
         # add widgets to layout
-        for w in (open_image, open_folder, prev_image, next_image):
-            layout.addWidget(w.native)
+        layout.addWidget(open_image.native)
 
     def _clear_auto_seg(self):
         if "auto_seg" in self.viewer.layers:
