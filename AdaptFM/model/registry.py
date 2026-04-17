1a03dee0f2e4b17b1e1ed94c66fd255b48510e1c
hbakhtiar <hbakhtiar@wisc.edu>
2026-04-17 17:48:54 -0500

    Update registry.py

--- AdaptFM/model/registry.py
+++ AdaptFM/model/registry.py
@@ -39,11 +39,11 @@ MODEL_REGISTRY = {
         training_wrapper_path="AdaptFM.model.foundation_models.SSVT.train_wrapper",
         inference_wrapper_path="AdaptFM.model.foundation_models.SSVT.inference_wrapper"
     ),
+    
+}
 
-    "SAM2": {"Checkpoint Path" : '/path/to/SAM2/checkpoints/',
+SAM_REGISTRY={   "SAM2": {"Checkpoint Path" : '/path/to/SAM2/checkpoints/',
             "Repo Root": 'path/to/SAM2/repo/'},
 
     "SAM3": {"Checkpoint Path" : '/path/to/SAM2/checkpoints/',
-        "Repo Root": 'path/to/SAM2/repo/'}
-    
-}
+        "Repo Root": 'path/to/SAM2/repo/'}}
