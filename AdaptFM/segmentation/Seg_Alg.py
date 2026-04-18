b0da8ce97e9cedf5793ebdad4568de584485d1ea
hbakhtiar <hbakhtiar@wisc.edu>
2026-04-18 10:17:49 -0500

    Update Seg_Alg.py

--- AdaptFM/segmentation/Seg_Alg.py
+++ AdaptFM/segmentation/Seg_Alg.py
@@ -3,7 +3,6 @@ from AdaptFM.segmentation.fourier.cell_seg import run_single_cell_segmentation
 from AdaptFM.segmentation.fourier.org_seg import run_organoid_segmentation
 from AdaptFM.segmentation.fourier.nuc_seg_gpu import run_nuclear_segmentation_gpu_chunked
 from AdaptFM.segmentation.checkpoint_utils import ensure_sam2_checkpoints,check_sam2_installed
-from AdaptFM.model.registry import SAM_REGISTRY
 from abc import ABC
 
 class SegmentationAlgorithmSpec(ABC):
