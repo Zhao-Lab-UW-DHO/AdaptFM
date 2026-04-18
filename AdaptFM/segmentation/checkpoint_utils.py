0ad1e84569fa9b05786492701386967b56adb612
hbakhtiar <hbakhtiar@wisc.edu>
2026-04-18 10:05:03 -0500

    Create checkpoint_utils.py

---
+++ AdaptFM/segmentation/checkpoint_utils.py
@@ -0,0 +1,37 @@
+import urllib.request
+from pathlib import Path
+
+SAM2_CHECKPOINTS = {
+    "sam2_hiera_tiny.pt":
+        "https://dl.fbaipublicfiles.com/segment_anything_2/092824/sam2.1_hiera_tiny.pt",
+
+    "sam2_hiera_small.pt":
+        "https://dl.fbaipublicfiles.com/segment_anything_2/092824/sam2.1_hiera_small.pt",
+
+    "sam2_hiera_base.pt":
+        "https://dl.fbaipublicfiles.com/segment_anything_2/092824/sam2.1_hiera_base_plus.pt",
+
+    "sam2_hiera_large.pt":
+        "https://dl.fbaipublicfiles.com/segment_anything_2/092824/sam2.1_hiera_large.pt",
+}
+
+def download_file(url, dest):
+    print(f"Downloading {dest.name}...")
+
+    urllib.request.urlretrieve(url, dest)
+
+
+def ensure_sam2_checkpoints():
+    ckpt_dir = get_checkpoint_dir() / "sam2"
+
+    ckpt_dir.mkdir(parents=True, exist_ok=True)
+
+    for name, url in SAM2_CHECKPOINTS.items():
+
+        dest = ckpt_dir / name
+
+        if not dest.exists():
+
+            download_file(url, dest)
+
+    return ckpt_dir
