1ee357c848556276dfe3fc9d25837412c2ee0bb9
hbakhtiar <hbakhtiar@wisc.edu>
2026-04-20 08:08:22 -0500

    Update benchmark_widget.py

--- AdaptFM/gui/widgets/benchmark_widget.py
+++ AdaptFM/gui/widgets/benchmark_widget.py
@@ -1,5 +1,10 @@
 from napari.qt.threading import thread_worker
 from qtpy.QtWidgets import QWidget, QVBoxLayout, QComboBox, QPushButton, QLabel, QFileDialog, QListWidget
+from qtpy.QtWidgets import QSpinBox
+import subprocess
+import json
+import multiprocessing
+import sys
 from AdaptFM.gui.widgets.metrics_widget import MetricRegistry
 
 class BenchmarkWidget(QWidget):
@@ -39,6 +44,21 @@ class BenchmarkWidget(QWidget):
         self.compute_button = QPushButton("Compute Metric")
         self.compute_button.clicked.connect(self.compute_metrics)
         self.layout.addWidget(self.compute_button)
+        self.layout.addWidget(QLabel("Number of Processes"))
+
+        self.nproc_spinbox = QSpinBox()
+
+        self.nproc_spinbox.setMinimum(1)
+
+        self.nproc_spinbox.setMaximum(
+            multiprocessing.cpu_count()
+        )
+
+        self.nproc_spinbox.setValue(
+           1
+        )
+
+        self.layout.addWidget(self.nproc_spinbox)
 
     def select_ground_truth(self):
         self.gt_dir = QFileDialog.getExistingDirectory(None, "Select ground truth")
@@ -55,15 +75,37 @@ class BenchmarkWidget(QWidget):
             if selected >= 0:
                 self.models_list.takeItem(selected)
                 self.models_dirs.pop(selected)
+            
+
 
     def compute_metrics(self):
         metric_name = self.metric_dropdown.currentText()
-        metric = MetricRegistry.create(metric_name)
 
         if not self.gt_dir or not self.models_dirs:
             print("Select ground truth and at least one model!")
             return
 
-        # just pass paths to the metric
-        metric.compute(self.gt_dir, self.models_dirs)
+        models_json = json.dumps(self.models_dirs)
         
+        num_processes = str(
+        self.nproc_spinbox.value()
+        )
+
+        cmd = [
+            sys.executable,
+            "-m","AdaptFM.gui.run_metric_subprocess",
+            "--metric", metric_name,
+            "--gt_dir", self.gt_dir,
+            "--models_json", models_json,
+            "--num_processes",num_processes
+        ]
+
+        print("Launching metric subprocess...")
+
+        self.proc = subprocess.Popen(
+            cmd,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )
+
+
