0e583b6f08bda35d6e92d9ab0ebb000ce7a58030
hbakhtiar <hbakhtiar@pr-szhao-01.platformr.wisc.edu>
2026-06-25 11:13:49 -0500

    Update dev_launch.py

--- AdaptFM/dev_launch.py
+++ AdaptFM/dev_launch.py
@@ -9,6 +9,8 @@ from AdaptFM.session.annotation_session import AnnotationSession
 from AdaptFM.gui.widgets.inference_widget import InferenceWidget
 from AdaptFM.gui.widgets.training_widget import TrainingWidget
 from AdaptFM.gui.widgets.benchmark_widget import BenchmarkWidget
+from AdaptFM.gui.widgets.postprocess_widget import PostProcessWidget
+from AdaptFM.gui.widgets.env_manager_dialog import EnvironmentManagerDialog
 from AdaptFM.model.registry import MODEL_REGISTRY
 from qtpy.QtWidgets import QAction
 
@@ -68,6 +70,36 @@ def main():
 
     # Show widget when menu action triggered
     benchmark_action.triggered.connect(benchmark_widget.show)
+
+    menu = viewer.window._qt_window.menuBar().addMenu("Post Process")
+    postprocess_action = QAction("Run Post processing", viewer.window._qt_window)
+    menu.addAction(postprocess_action)
+
+    # Lazy-create widget
+    postprocess_widget = PostProcessWidget(dataset_manager=dm).widget
+
+    # Show widget when menu action triggered
+    postprocess_action.triggered.connect(postprocess_widget.show)
+
+    # ------------------------------------------------------------------ #
+    # Environments menu  ← NEW
+    # ------------------------------------------------------------------ #
+    env_menu = viewer.window._qt_window.menuBar().addMenu("Environments")
+    env_action = QAction("Manage Environments…", viewer.window._qt_window)
+    env_menu.addAction(env_action)
+ 
+    # Lazy-create: dialog is parented to the main window so it stays on top
+    _env_dialog: list[EnvironmentManagerDialog] = []   # mutable cell
+ 
+    def _open_env_manager():
+        if not _env_dialog:
+            dlg = EnvironmentManagerDialog(parent=viewer.window._qt_window)
+            _env_dialog.append(dlg)
+        _env_dialog[0].show()
+        _env_dialog[0].raise_()
+        _env_dialog[0].activateWindow()
+ 
+    env_action.triggered.connect(_open_env_manager)
 
 
     napari.run()
