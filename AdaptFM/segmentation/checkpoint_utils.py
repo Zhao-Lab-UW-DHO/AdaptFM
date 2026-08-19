cbdfaf7c41ad2230469ef274faecac65e5bca063
hbakhtiar <hbakhtiar@wisc.edu>
2026-08-19 13:28:07 -0500

    Update SAM2, SAM3 errors

--- AdaptFM/segmentation/checkpoint_utils.py
+++ AdaptFM/segmentation/checkpoint_utils.py
@@ -3,23 +3,29 @@ import sys
 import importlib
 
 
-def check_sam2_installed():
 
+def check_sam2_installed():
     try:
         import sam2
-
+        return True
     except ImportError:
+        raise RuntimeError(
+            "SAM2 is not installed.\n"
+            "You must install with\n"
+            "the environment manager"
+        )
+    
 
-        try:
-            if "sam2" in sys.modules:
-                importlib.reload(sys.modules["sam2"])
-        except:
-            raise RuntimeError(
-                "SAM2 is not installed.\n"
-                "Install with:\n"
-                "the environment manager"
-            )
-
+def check_sam3_installed():
+    try:
+        import sam3
+        return True
+    except ImportError:
+        raise RuntimeError(
+            "SAM3 is not installed.\n"
+            "You must install with\n"
+            "the environment manager"
+        )
 
 
 def download_file(url, dest):
